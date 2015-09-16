37af216d0d57a15024d7d10ac8016f913e9424b7
Arthur Rapaport <artra304@student.liu.se>
2015-09-16 12:12:12 +0200

    Added google analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,6 +12,16 @@
 	<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
 	<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
 	<link rel="stylesheet" type="text/css" href="css/all.css">
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-67686247-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 </head>
 <body>
 
